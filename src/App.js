c3972fece364519c25d9026017fb17fa0ad80768
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-27 12:37:56 +0900

    feat: 토글, 색상 랜덤 변경, 더하기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
 function App() {
+  const [randomColor, setRandomColor] = useState('');
+  const [count, setCount] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const getRandomColor = () => {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  };
+
   return (
     <div>
-      Create React App
+      <button onClick={() => setVisible(!visible)}>토글</button>
+      {visible ? <div>
+        <button onClick={() => setRandomColor(getRandomColor())}>랜덤 색상</button>
+        <button onClick={() => setCount(count + 1)}>더하기</button>
+        <h1 style={{ color: randomColor }}>{count}</h1>
+        <p>color : {randomColor}</p>
+      </div> : null}
     </div>
   );
 }
